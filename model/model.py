7b1745e37dd9de5825ed7da46152b7aa86d335bc
jayceyao <jayceyao@tencent.com>
2025-10-17 16:22:25 +0800

    feat: model包

---
+++ model/model.py
@@ -0,0 +1,19 @@
+from peewee import *
+from db.db import db
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Person(BaseModel):
+    id = AutoField()
+    name = CharField()
+    birthday = DateField()
+
+
+class Pet(BaseModel):
+    owner = ForeignKeyField(model=Person, field=Person.id, backref="pets")
+    name = CharField()
+    animal_type = CharField()
