7141fd62c7ce52fa4b87ec58ba30bd676c271199
jayceyao <jayceyao@tencent.com>
2025-10-23 15:56:39 +0800

    feat: 主表记录删除，级联删除从表记录

--- example/delete_data.py
+++ example/delete_data.py
@@ -1,4 +1,7 @@
-from model.model import Pet
+from model.model import Pet, Person
 
-herb_mittens = Pet.get(Pet.name == "Mittens")
-herb_mittens.delete_instance()
+"""
+在pet模型中定义外键时指定了on_delete="CASCADE"，所以，此时删除person，会把关联到的pet也删除
+"""
+uncle_bob = Person.get(Person.name == "Bob")
+uncle_bob.delete_instance(recursive=True)
